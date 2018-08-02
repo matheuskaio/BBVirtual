ab63d252be6fb7aff960c8c4ee373947b41d6803
matheuskaio <m.k.m.p.2000@gmail.com>
2018-08-02 20:28:19 -0300

    Cores no botão

--- app/src/main/java/com/example/matheus/meuprojeto/CadastrarUsuario.java
+++ app/src/main/java/com/example/matheus/meuprojeto/CadastrarUsuario.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 
 public class CadastrarUsuario extends AppCompatActivity {
@@ -11,8 +13,8 @@ public class CadastrarUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrar_usuario);
 
+        setContentView(R.layout.activity_cadastrar_usuario);
         TabHost host =  findViewById(R.id.tabhost);
         host.setup();
         //tab 1
